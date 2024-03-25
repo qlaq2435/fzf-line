c638f3b486589c4f72e69180e84767ca277f04b0
qlaq2435 <qlaq2435@outlook.com>
2024-03-25 17:22:59 +0800

    implement getActiveEditorRelativePath

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
+import path from 'path';
 // Your extension is activated the very first time the command is executed
+// This method is called when your extension is activated
+function getActiveEditorRelativePath() {
+	let activeTextEditor = vscode.window.activeTextEditor ;
+	if (activeTextEditor !== undefined){
+		var uri = activeTextEditor.document.uri;
+		var relativePath = vscode.workspace.asRelativePath(uri);
+
+		return relativePath;
+	}
+	else {
+		vscode.window.showInformationMessage('Active Text Editor not found, open a text and try again');
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -14,6 +27,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 	//Write to output.
 	orange.appendLine('Congratulations, your extension "fzf-line" is now active!');
+	orange.appendLine('if use pwsh, the version of pwsh had better higher then 7.4.1 for avoid the chinese text garbled');
+	let relativePath = getActiveEditorRelativePath();
+	if (relativePath !== undefined)
+	{
+		let message = `relative path: ${relativePath}` ;
+		vscode.window.showInformationMessage(message);
+	}
+
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
