f376c648d8dcd3573bd28ddd6cb85fa0e3415684
qlaq2435 <qlaq2435@outlook.com>
2024-03-27 03:46:03 +0800

    plugin publish

--- src/extension.ts
+++ src/extension.ts
@@ -43,7 +43,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fzf-line.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('fzf-line.fzf-match-lines', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		let activeEditor = utils.getActiveEditor();
